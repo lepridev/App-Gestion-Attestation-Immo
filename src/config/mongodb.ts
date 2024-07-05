22cc6beea5cc0a5c761022b8e8382385ab50b460
lepridev <lepridev35@gmail.com>
2024-07-05 23:03:44 +0000

    Correction 1

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,12 +1,21 @@
 import mongoose from "mongoose";
 
+let isConnected = false; // Suivi de l'état de la connexion
+
 export const connectToDB = async () => {
+  if (isConnected) {
+    console.log("Already connected to the database.");
+    return;
+  }
+
   try {
-    const connexion = await mongoose.connect(process.env.MONGO_DB_URI!);
-    if (connexion.ConnectionStates.connected) {
-      console.log("Connected !");
+    const db = await mongoose.connect(process.env.MONGO_DB_URI!);
+
+    isConnected = db.connections[0].readyState === 1; // Vérifie si la connexion est établie
+    if (isConnected) {
+      console.log("Connected to the database!");
     }
   } catch (error) {
-    console.log("Some thing went wrong", error);
+    console.error("Something went wrong", error);
   }
 };
